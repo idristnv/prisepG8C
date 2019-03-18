0cd5d5ffca3d2c0c46f96cea52f2e1fee85bafeb
MykolaChoban <mykola.choban@isep.fr>
2019-03-18 09:20:30 +0100

    j'ai ajouté le site et la forme

---
+++ iframeConnection.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<!-- https://www.w3schools.com/w3css/w3css_input.asp -->
+<head>
+  <title>Prisep Connexion</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body style="background: white;">
+  <form class="w3-container" >
+    <br>
+    <label>Adresse mail</label>
+    <input class="w3-input w3-animate-input  w3-hover-grey" style="width:50%" type="text">
+
+    <label>Mot de passe</label>
+    <input class="w3-input w3-animate-input  w3-hover-grey" style="width:50%" type="text">
+    <br>
+    <p><a href="">mot de passe oublié?</a></p>
+    <br>
+    <button class="w3-btn w3-blue">Se connecter</button>
+  </form>
+</body>
+</html>
---
+++ iframeRegister.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<!-- https://www.w3schools.com/w3css/w3css_input.asp -->
+<head>
+  <title>Prisep Connexion</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body style="background: white;">
+  <form class="w3-container">
+    <br>
+    <label>Nom</label>
+    <input class="w3-input w3-animate-input  w3-hover-grey" style="width:50%" type="text">
+
+    <label>Prénom</label>
+    <input class="w3-input w3-animate-input  w3-hover-grey" style="width:50%" type="text">
+
+    <label>Adresse mail</label>
+    <input class="w3-input w3-animate-input  w3-hover-grey" style="width:50%" type="text">
+
+    <label>Date de naissance</label>
+    <input class="w3-input   w3-hover-grey"  style="width: 180px" type="Date" min="1900-12-31" max="2000-12-31"">
+
+    <label>Mot de passe</label>
+    <input class="w3-input w3-animate-input  w3-hover-grey" style="width:50%" type="text">
+    <p><a href="gcu.html" style="color: blue;">Conditions générales d'utilisation</a></p>
+    <input type="checkbox" name=checkboxGcu>j'ai lu et j'accepte les conditions générales d'utilisation<br>
+    </br>
+    <button class="w3-btn w3-blue">confirmer l'inscription</button>
+
+</form>
+
+
+</body>
+</html>
