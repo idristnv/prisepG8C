0288ed2472b2b801060a5eb6ccca82ca25c1c05d
MykolaChoban <mykola.choban@isep.fr>
2019-03-25 10:24:52 +0100

    j'ai changé l'extension de certain fichier

--- pageRegisterLogin.php
+++ pageRegisterLogin.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+$_SESSION['prenom'] = 'Jean';
+$_SESSION['age'] = 24;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
--- pageUserMenu.php
+++ pageUserMenu.php
@@ -1,3 +1,7 @@
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +9,8 @@
   <title>Document</title>
 </head>
 <body>
-  <?php include("barre de navigation.html"); ?>
+  <?php include("barre de navigation.html");
+  ?>
 
 
 </body>
