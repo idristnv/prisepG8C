d23d621f36452b8efbdeb2685136ac032d561e7d
idristnv <47316405+idristnv@users.noreply.github.com>
2019-04-10 11:07:01 +0200

    Update pagePasswordForgotten.php
    j'ai ajouter le code pour envoyer un mail

---
+++ pagePasswordForgotten.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+if(!empty($_POST)){
+    extract($_POST);
+    $valid = true;
+    if (isset($_POST['oublie'])){
+        $mail = htmlentities(strtolower(trim($mail))); // On récupère le mail afin d envoyer le mail pour la récupèration du mot de passe 
+
+        // Si le mail est vide alors on ne traite pas
+        if(empty($mail)){
+            $valid = false;
+            $er_mail = "Il faut mettre un mail";
+        }
+// On génère un mot de passe à l'aide de la fonction RAND de PHP
+                        $new_pass = rand();
+
+                        // Le mieux serait de générer un nombre aléatoire entre 7 et 10 caractères (Lettres et chiffres)
+                        $new_pass_crypt = crypt($new_pass, "$6$rounds=5000$macleapersonnaliseretagardersecret$");
+                        // $new_pass_crypt = crypt($new_pass, "VOTRE CLÉ UNIQUE DE CRYPTAGE DU MOT DE PASSE");
+
+                        $objet = 'Nouveau mot de passe';
+                        $to = $_POST['mail'];
+
+                        //===== Création du header du mail.
+                        $header = "From: Prisep <no-reply@test.com> \n";
+                        $header .= "Reply-To: ".$to."\n";
+                        $header .= "MIME-version: 1.0\n";
+                        $header .= "Content-type: text/html; charset=utf-8\n";
+                        $header .= "Content-Transfer-Encoding: 8bit";
+
+                        //===== Contenu de votre message
+                        $contenu =  "<html>".
+                            "<body>".
+                            "<p style='text-align: center; font-size: 18px'><b>Bonjour</b>,</p><br/>".
+                            "<p style='text-align: justify'><i><b>Nouveau mot de passe : </b></i>".$new_pass."</p><br/>".
+                            "</body>".
+                            "</html>";
+                        //===== Envoi du mail
+                        mail($to, $objet, $contenu, $header);
+                        exit;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Mot de passe oublié</title>
+    </head>
+    <body>
+        <div>Mot de passe oublié</div>
+        <form method="post">
+            <?php
+                if (isset($er_mail)){
+            ?>
+                <div><?= $er_mail ?></div>
+            <?php   
+                }
+            ?>
+            <input type="email" placeholder="Adresse mail" name="mail" value="<?php if(isset($mail)){ echo $mail; }?>" required>
+            <button type="submit" name="oublie">Envoyer</button>
+        </form>
+    </body>
+</html>
