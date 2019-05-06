864865c39ea3193fb99b79ba9e5fa411be93e553
valnajean <valhk98@gmail.com>
2019-05-06 10:22:49 +0200

    Add files via upload
    Le SAV

---
+++ minichat.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mini-chat</title>
+    </head>
+    <style>
+    form
+    {
+        text-align:center;
+    }
+    </style>
+    <body>
+    
+    <form action="minichat_post.php?ticket=<?php $_GET['ticket'] ?>" method="post">
+        <p>
+        <!-- <label for="pseudo">Pseudo</label> : <input type="text" name="adresseMail" id="pseudo" /><br /> -->
+        <label for="message">Message</label> :  <input type="text" name="texte" id="message" /><br />
+        <input type="hidden" name="numTicketURL" value="<?php echo $_GET['ticket'] ?>" />
+        <input type="submit" value="Envoyer" />
+	</p>
+    </form>
+
+<?php
+// Connexion à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=requetes;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+// Récupération des derniers messages
+$reponse = $bdd->query('SELECT adresseMail, texte, numTicket FROM messgae ORDER BY messageNB ASC');
+//$reponse = $bdd->query('SELECT * FROM messgae');
+//$ticket = $bdd->query('SELECT mailUser FROM ticket ORDER BY numTicket ASC');
+
+
+
+// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+while ($donnees = $reponse->fetch()){
+    if ($donnees['adresseMail'] == 'valentinnajean@isep.fr' and $donnees['numTicket'] == $_GET["ticket"])
+{
+	echo '<p><strong>' . $donnees['adresseMail'] . '</strong> : ' . $donnees['texte'] . '</p>';
+}
+}
+$reponse->closeCursor();
+
+?>
+    </body>
+</html>
---
+++ newticket.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Creation Ticket</title>
+    </head>
+    <style>
+    form
+    {
+        text-align:center;
+    }
+    h1 
+    {
+        text-align:center;
+    }
+    h2
+    {
+        text-align:center;
+    }
+    .divTicketex
+    {
+        display:block;
+        text-align:center;
+        margin:auto;
+    }
+    </style>
+    <body>
+    <h1>Requetes</h1>
+    <h2>Nouveau tickets</h2>
+    <form action="newticket_post.php" method="post">
+        <p>
+        <!-- <label for="pseudo">Pseudo</label> : <input type="text" name="adresseMail" id="pseudo" /><br /> -->
+        <!-- <label for="message">Message</label> :  <input type="text" name="texte" id="message" /><br /> -->
+
+        <input type="submit" name="bouton ticket" value="Creer un ticket"/>
+    
+	</p>
+    </form>
+
+    <h2>Vos tickets existants</h2>
+    <!-- <div style="background-color:grey"> -->
+ 
+    <?php
+
+    try
+    {
+	    $bdd = new PDO('mysql:host=localhost;dbname=requetes;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+
+
+
+    $listTicket = $bdd->query('SELECT numTicket, mailUser FROM ticket ORDER BY numTicket ASC');
+   
+while($donnTick = $listTicket->fetch() /*and $donntick['mailUser']=='valentinnajean@isep.fr'*/){     //a remplacer par $_SESSION[addresse mail]
+    if ($donnTick['mailUser'] == 'valentinnajean@isep.fr'){
+    ?>
+    <div class="divTicketex">
+        <a href="minichat.php?ticket=<?php echo $donnTick['numTicket'] ?>" >Ticket numero <?php echo $donnTick['numTicket'] ?></a>
+        <!-- echo '<p>' . $donntick['numTicket']. '</p>';  METHOD GET-->
+
+
+
+    </div>
+    <?php
+    }
+}
+$listTicket->closeCursor();
+?>
+    <!-- </div> -->
+</body>
+</html>
+
+
