8d8a10e73f3ade47f1315635246f9340d66da289
Théo <theo.esquerre@hotmail.fr>
2019-05-10 14:40:45 +0200

    Theo FAQ

--- pageFAQ.php
+++ pageFAQ.php
@@ -8,9 +8,25 @@
     <?php include("barre de navigation.html");?>
     <body>
         <h1>FAQ</h1>
-        <div>
+        <br>
+        <br>
+        <br>
+        <h2>Comment gérer mon compte Prisep?</h2>
+        <br>
+        <p>Il vous faut tout d'abord créer un compte client. Une fois le compte créé, vous devez définir votre adresse et les différentes pièces où se situent les multiprises.</p>
+        <br>
+        <h3>Est-il possible d'avoir plusieurs multiprises dans un même pièce?</h3>
+        <br>
+        <p>Oui bien entendu, il suffit de cliquer sur le bouton "+".</p>
+        <br>
+        <h4>Est-il possible d'indiquer plusieurs domiciles?</h4>
+        <br>
+        <p>Oui, tout comme pour les multiprises, il s'agit de la même méthode d'ajout.</p>
+        <br>
+        <h5>En cas de problème, qui contacter?</h5>
+        <br>
+        <p>Dans la barre de naivigation, cliquez sur "Besoin d'aide?", créez ensuite un ticket incident et renseignez votre problème. Un administrateur vous répondra dans les plus brefs délais.</p>
 
-        </div>
     </body>
     <?php include("Footer.html") ?>
 </html>
