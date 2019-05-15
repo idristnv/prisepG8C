941086e28e1413b93e54a60980a10fcd45909f1b
Idris <idris.tinval@gmail.com>
2019-05-15 10:22:59 +0200

    Update pagePasswordForgotten.php

--- pagePasswordForgotten.php
+++ pagePasswordForgotten.php
@@ -1,20 +1,24 @@
 <?php include("barre de navigation.html"); ?>
-
 <?php
-session_start();
-if(!empty($_POST)){
-    extract($_POST);
-    $valid = true;
-    if (isset($_POST['oublie'])){
-        $mail = htmlentities(strtolower(trim($mail))); // On récupère le mail afin d envoyer le mail pour la récupèration du mot de passe
+    session_start();
+    $bdd = new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root','root');
 
-        // Si le mail est vide alors on ne traite pas
-        if(empty($mail)){
-            $valid = false;
-            $er_mail = "Il faut mettre un mail";
-        }
-                        $new_pass = rand(); // On génère un mot de passe à l'aide de la fonction RAND de PHP
+    }
 
+    if(!empty($_POST)){
+        extract($_POST);
+        $valid = true;
+
+        if (isset($_POST['oublie'])){
+            $mail = htmlentities(strtolower(trim($mail))); // On récupère le mail afin d envoyer le mail pour la récupèration du mot de passe 
+
+            // Si le mail est vide alors on ne traite pas
+            if(empty($mail)){
+                $valid = false;
+                $er_mail = "Il faut mettre un mail";
+            }
+                        // On génère un mot de passe à l'aide de la fonction RAND de PHP
+                        $new_pass = rand();
 
                         // Le mieux serait de générer un nombre aléatoire entre 7 et 10 caractères (Lettres et chiffres)
                         $new_pass_crypt = crypt($new_pass, "$6$rounds=5000$macleapersonnaliseretagardersecret$");
@@ -31,19 +35,22 @@ if(!empty($_POST)){
                         $header .= "Content-Transfer-Encoding: 8bit";
 
                         //===== Contenu de votre message
-                        $contenu = 
-                         "<html>".
+                        $contenu =  "<html>".
                             "<body>".
-                            <img src="styleSheet/image/logoSite"
-                            "<p style='text-align: center; font-size: 18px'><b>Bonjour</b>,</p><br/>".
-                            "<p style='text-align: justify'><i><b>Voici votre nouveau mot de passe : </b></i>".$new_pass."</p><br/>".
+                            "<p style='text-align: center; font-size: 18px'><b>Bonjour Mr, Mme".$verification_mail['nom']."</b>,</p><br/>".
+                            "<p style='text-align: justify'><i><b>Voic vouveau mot de passe : </b></i>".$new_pass."</p><br/>".
                             "</body>".
                             "</html>";
                         //===== Envoi du mail
                         mail($to, $objet, $contenu, $header);
-                        exit;
+                        $DB->insert("UPDATE utilisateur SET mdp = ?, n_mdp = 1 WHERE mail = ?", 
+                            array($new_pass_crypt, $verification_mail['mail']));
+                    }   
+                }       
+                exit;
+            }
+        }
     }
-}
 ?>
 
 <!DOCTYPE html>
