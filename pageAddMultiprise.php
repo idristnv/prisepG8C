b0ea01f37461d81d0112298421ed5e82ba2066f0
Idris <idris.tinval@gmail.com>
2019-05-20 11:07:00 +0200

    Update pageAddMultiprise.php

--- pageAddMultiprise.php
+++ pageAddMultiprise.php
@@ -31,7 +31,7 @@
 
                 <label> Numero de série :</label>
                 <input type="text" name="NumeroDeSerie" required
-                minlength="4" maxlength="200" size="50" placeholder="Saisissez le numéro de série de votre multiprise">
+                minlength="4" maxlength="200" size="50" placeholder="Saisissez le numéro de série de votre multiprise" font-size=1.4em>
 
             <br/> <br/> <br/>
 
